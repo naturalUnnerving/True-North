24890ff388862193d6441fb3899c702f8e38b970
Ilia Merkulov <77540282+IlyaM70@users.noreply.github.com>
2024-11-16 13:17:10 -0800

    Game end screens
    Added game over screen
    Added victory screen
    Moved all screens logic to one class
    Added redirection to game over and victory screen in battle logic

---
+++ Assets/Scripts/ScreenFuntions.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScreenFuntions : MonoBehaviour
+{
+    [SerializeField] private string newGameScene;
+    [SerializeField] private string optionsScene;
+    [SerializeField] private string titleScreenScene;
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(newGameScene);
+    }
+    public void Options()
+    {
+        SceneManager.LoadScene(optionsScene);
+    }
+
+    public void TitleScreen()
+    {
+        SceneManager.LoadScene(titleScreenScene);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
