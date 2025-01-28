55f84b459f40acfc038471da2fa9f53b5b5f4cc4
HungBDao <bachung2005@gmail.com>
2025-01-28 15:26:19 -0500

    Finished the options menu

--- Assets/Scripts/SettingsScript.cs
+++ Assets/Scripts/SettingsScript.cs
@@ -1,13 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsScript : MonoBehaviour
 {
     public AudioMixer audioMixer;
+
+    public TMP_Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+
+    void Start() {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
     public void SetVolume(float volume)
     {
         audioMixer.SetFloat("mainVolume", volume);
+    }
+
+    public void SetFullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 }
